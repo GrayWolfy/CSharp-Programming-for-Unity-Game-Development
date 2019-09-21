832bf1fe29f0cd41b42d15162612870a34f02a42
sergeypopov23 <sergeypopov23@gmail.com>
2019-09-21 23:13:56 +0300

    Math exercise

---
+++ Exercise3/Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = 30;
+            double cosine = Math.Cos(degrees * Math.PI / 180);
+            Console.WriteLine(cosine);
+        }
+    }
+}
